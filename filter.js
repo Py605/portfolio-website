110cb5293c31d5e2a9ada37cd9c371983660f29e
Priyanshu Bharti <pryanshubharti9412@gmail.com>
2022-09-19 17:47:39 +0530

    Full length Home section &
    Full length home section with sticky nav(with the help of js) + minor font changes...

--- filter.js
+++ filter.js
@@ -1,3 +1,19 @@
+// for Navbar..
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+
 // ---Project Section---
 let sortBtn = document.querySelector('.filter-menu').children;
 let sortItem = document.querySelector('.filter-item').children;
